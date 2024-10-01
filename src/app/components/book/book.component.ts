790b91b68abe09fd9737dbc8a93dd548c9d6f86c
jerome09232 <jeromecberiso87@gmail.com>
2024-10-01 21:17:12 +0900

    booklist

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './book.component.css'
 })
 export class BookComponent {
+  bookList: string[] = ["The Lord of the Rings", "Pride and Prejudice", "To Kill a Mockingbird"];
+  item: string = '';
 
+  addItems() {
+    this.bookList.push(this.item);
+  }
+
+  deleteBook(index: number) {
+    this.bookList.splice(index, 1);
+  }
 }
