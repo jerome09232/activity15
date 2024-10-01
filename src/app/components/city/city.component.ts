f6304ff9a2e9cdbc7c04533e0bf548765b74950d
jerome09232 <jeromecberiso87@gmail.com>
2024-10-01 21:20:48 +0900

    citylist

--- src/app/components/city/city.component.ts
+++ src/app/components/city/city.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './city.component.css'
 })
 export class CityComponent {
+  cityList: string[] = ["London", "Paris", "New York"];
+  item: string = '';
 
+  addItems() {
+    this.cityList.push(this.item);
+  }
+
+  deleteCity(index: number) {
+    this.cityList.splice(index, 1);
+  }
 }
