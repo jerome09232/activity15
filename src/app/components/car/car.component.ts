bbe52daeebec3cad8b723a2ff636c71d4baabfcd
jerome09232 <jeromecberiso87@gmail.com>
2024-10-01 21:35:59 +0900

    carmodellist

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './car.component.css'
 })
 export class CarComponent {
+  carModelList: string[] = ["Toyota Camry", "Honda Civic", "Ford Mustang"];
+  item: string = '';
 
+  addItems() {
+    this.carModelList.push(this.item);
+  }
+
+  deleteCarModel(index: number) {
+    this.carModelList.splice(index, 1);
+  }
 }
