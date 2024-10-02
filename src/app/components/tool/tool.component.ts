fed3efd625712795cc9d0a85c7a0308bb214769b
jerome09232 <jeromecberiso87@gmail.com>
2024-10-02 12:35:08 +0900

    toollist

--- src/app/components/tool/tool.component.ts
+++ src/app/components/tool/tool.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './tool.component.css'
 })
 export class ToolComponent {
+  toolList: string[] = ["Hammer", "Screwdriver", "Wrench"];
+  item: string = '';
 
+  addItems() {
+    this.toolList.push(this.item);
+  }
+
+  deleteTool(index: number) {
+    this.toolList.splice(index, 1);
+  }
 }
