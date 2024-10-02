39d0f10e8a9013040cdf1f3d43665fdb653c41fc
jerome09232 <jeromecberiso87@gmail.com>
2024-10-02 12:37:37 +0900

    laguagelist

--- src/app/components/laguage/laguage.component.ts
+++ src/app/components/laguage/laguage.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './laguage.component.css'
 })
 export class LaguageComponent {
+  languageList: string[] = ["English", "Spanish", "French"];
+  item: string = '';
 
+  addItems() {
+    this.languageList.push(this.item);
+  }
+
+  deleteLanguage(index: number) {
+    this.languageList.splice(index, 1);
+  }
 }
