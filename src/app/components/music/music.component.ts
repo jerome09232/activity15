5d35dad8e984aded27e3ece7629d67bc7cfea349
jerome09232 <jeromecberiso87@gmail.com>
2024-10-02 12:48:26 +0900

    musicplaylist

--- src/app/components/music/music.component.ts
+++ src/app/components/music/music.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './music.component.css'
 })
 export class MusicComponent {
+  playlist: string[] = ["Bohemian Rhapsody", "Imagine", "Hotel California"];
+  newSong: string = '';
 
+  addSong() {
+    this.playlist.push(this.newSong);
+    this.newSong = ''; // Clear the input field
+  }
+
+  deleteSong(index: number) {
+    this.playlist.splice(index, 1);
+  }
 }
