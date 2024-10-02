2e6ad3c7a0e3dd9ab127028f39e2b87d3f90392d
jerome09232 <jeromecberiso87@gmail.com>
2024-10-02 12:57:06 +0900

    grocerylist

--- src/app/components/grocery/grocery.component.ts
+++ src/app/components/grocery/grocery.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-grocery',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './grocery.component.html',
   styleUrl: './grocery.component.css'
 })
 export class GroceryComponent {
+  groceryList: string[] = ["Milk", "Eggs", "Bread", "Cheese"];
+  newItem: string = '';
 
+  addItem() {
+    this.groceryList.push(this.newItem);
+    this.newItem = ''; // Clear the input field
+  }
+
+  deleteItem(index: number) {
+    this.groceryList.splice(index, 1);
+  }
 }
